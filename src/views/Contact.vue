383af68c52347b8f188aee11310fe82f1c2d04ce
Alina Kitaeva <akitaeva@gmail.com>
2020-04-23 12:45:58 -0400

    updates ui of the contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,23 +1,84 @@
 <template>
-  <div class="contact-info">
-    <div class="quick-contacts">
-      <h5>Customer Support</h5>
-      (855) 438-7379 support@repzio.com
-      <h5>Sales Assistance</h5>
-      (855) 438-7379 concierge@imcdi.com
-    </div>
-    <div class="addresses">
-      <h5>Find Us Here</h5>
-      790 Juno Ocean Walk Suite 402 Juno Beach, FL 33408 (855) 438-7379
-      contact@repzio.com Central Florida 2356 West Lake Mary, Blvd. Suite 1100
-      Lake Mary, FL 32746 (855) 438-7379 contact@repzio.com
+  <div class="page-wrapper">
+    <h2>Our Contacts</h2>
+    <div class="contact-info">
+      <div class="quick-contacts">
+        <div class="box">
+          <h4>Customer Support</h4>
+          <p>(855) 438-7379</p>
+          <p>support@repzio.com</p>
+        </div>
+
+        <div class="box">
+          <h4>Sales Assistance</h4>
+          <p>(855) 438-7379</p>
+          <p>concierge@imcdi.com</p>
+        </div>
+      </div>
+
+      <div class="addresses">
+        <div class="box">
+          <h5>South Florida</h5>
+          <p>790 Juno Ocean Walk Suite 402</p>
+          <p>Juno Beach, FL 33408</p>
+          <p>(855) 438-7379</p>
+          <p>contact@repzio.com</p>
+        </div>
+
+        <div class="box">
+          <h5>Central Florida</h5>
+          <p>2356 West Lake Mary, Blvd. Suite 1100</p>
+          <p>Lake Mary, FL 32746</p>
+          <p>(855) 438-7379</p>
+          <p>contact@repzio.com</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+<!-- TODO: Add a contact form for validation practice-->
+
 <style scoped lang="scss">
-.contact-info {
-  padding: 1rem 2rem;
+.page-wrapper {
+  padding: 2rem 3rem;
+  max-width: 900px;
+  width: 85vw;
   display: flex;
+  flex-direction: column;
+}
+.contact-info {
+  display: flex;
+  padding: 2rem 0;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+}
+
+.quick-contacts {
+  min-width: 15rem;
+}
+
+.addresses {
+  min-width: 15rem;
+}
+.box {
+  padding: 1rem 2rem;
+}
+
+h2 {
+  font-size: 1.8rem;
+  padding: 0 2rem;
+}
+
+h4 {
+  margin-bottom: 0.5rem;
+  font-size: 1.5rem;
+}
+
+h5 {
+  margin-bottom: 0.5rem;
+  font-size: 1.2rem;
 }
 </style>
